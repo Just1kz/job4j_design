2a55b334fb335c7f7c2b99f95a8407f53384c7f8
Just1lz <exelent.pride@rambler.ru>
2020-12-19 04:01:39 +0700

    1. Реализовать коллекцию Set на массиве [#409329] - добавил проверку на наличие элементов в массиве

--- src/main/java/ru/job4j/collection/SimpleSet.java
+++ src/main/java/ru/job4j/collection/SimpleSet.java
@@ -1,14 +1,16 @@
 package ru.job4j.collection;
 
 public class SimpleSet<T> {
-    private SimpleArray<T> rsl = new SimpleArray<T>();
+    private final SimpleArray<T> rsl = new SimpleArray<T>();
 
     public void add(T value) {
         boolean s = true;
-        while (rsl.iterator().hasNext()) {
-            if (rsl.iterator().next() == value) {
-                s = false;
-                break;
+        if(rsl.getLengthArray() != 0) {
+            while (rsl.iterator().hasNext()) {
+                if (rsl.iterator().next() == value) {
+                    s = false;
+                    break;
+                }
             }
         }
         if (s) {
