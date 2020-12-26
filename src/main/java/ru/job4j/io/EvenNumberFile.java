5e29d228141e5cf9675e78d883bf279440c3287e
Just1lz <exelent.pride@rambler.ru>
2020-12-26 16:58:33 +0700

    0.2. FileInputStream [#409362]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -11,16 +11,7 @@ public class EvenNumberFile {
             boolean rsl;
             while ((z = in.read()) != -1) {
 
-                if (z ==  48
-                        || z == 49
-                        || z == 50
-                        || z == 51
-                        || z == 52
-                        || z == 53
-                        || z == 54
-                        || z == 55
-                        || z == 56
-                        || z == 57) {
+                if (z >=  48 && z <= 57) {
                     String s = Character.toString((char) z);
                     rsl = (char) z % 2 == 0;
                     text
