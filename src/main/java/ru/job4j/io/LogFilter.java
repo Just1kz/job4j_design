710c3dfa61237dcb145f337212f9d084231bda6d
Just1lz <exelent.pride@rambler.ru>
2020-12-28 14:03:48 +0700

    0.3. BufferedReader. [#409353]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+    public static List<String> filter(String file) {
+        List<String> rsl = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
+            in.lines()
+                            .filter(x -> x.contains("404"))
+                            .forEach(rsl::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        System.out.println(log);
+    }
+}
