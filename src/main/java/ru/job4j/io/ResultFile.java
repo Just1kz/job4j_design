8f987224b145073baae3feea1bc92a171663ecd4
Just1lz <exelent.pride@rambler.ru>
2020-12-28 13:45:22 +0700

    0.1. FileOutputStream. [#409352]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,15 +5,11 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         int[][] rsl = new int[10][10];
-        for (int i = 1; i < rsl.length; i++) {
-            for (int z = 1; z < rsl[i].length; z++) {
-                rsl[i][z] = z * i;
-            }
-        }
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             out.write("Hello, job4j! ".getBytes());
             for (int i = 1; i < rsl.length; i++) {
                 for (int z = 1; z < rsl[i].length; z++) {
+                    rsl[i][z] = z * i;
                     String x = Integer.toString(rsl[i][z]) + " ";
                     out.write(x.getBytes());
                 }
