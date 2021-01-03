072c399bcd6d9b4692fc230cb7cfe9dd0241a250
Just1lz <exelent.pride@rambler.ru>
2021-01-03 20:46:19 +0700

    0. Видео [#409282]

---
+++ script.sql
@@ -0,0 +1,22 @@
+create database test1;
+
+create table test1(
+                      id serial primary key,
+                      name varchar(255),
+                      balance double precision,
+                      active boolean,
+                      note text
+);
+
+insert into test1.public.test1(name, balance, active, note) VALUES ('Anton', 100.50, true, 'Сотрудник Банка')
+
+select * from test1
+
+update test1.public.test1 set balance = 100000.50;
+
+select * from test1
+
+delete from test1.public.test1
+
+select * from test1
+
