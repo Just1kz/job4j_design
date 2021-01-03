08d92d2638a7a41d0cbaeeef7d0cc81f294da387
Just1lz <exelent.pride@rambler.ru>
2021-01-03 22:56:29 +0700

    some edit

--- script.sql
+++ script.sql
@@ -8,15 +8,15 @@ create table test1(
                       note text
 );
 
-insert into test1.public.test1(name, balance, active, note) VALUES ('Anton', 100.50, true, 'Сотрудник Банка')
+insert into test1.public.test1(name, balance, active, note) VALUES ('Anton', 100.50, true, 'Сотрудник Банка');
 
-select * from test1
+select * from test1;
 
 update test1.public.test1 set balance = 100000.50;
 
-select * from test1
+select * from test1;
 
-delete from test1.public.test1
+delete from test1.public.test1;
 
-select * from test1
+select * from test1;
 
