4a90f5f6b4f49c01833b8aef87904de84a998d71
Just1lz <exelent.pride@rambler.ru>
2021-01-20 01:35:14 +0700

    0. Видео [#409395]

---
+++ src/main/java/ru/job4j/ood/solid/ocp/ManagerGame.java
@@ -0,0 +1,9 @@
+package ru.job4j.ood.solid.ocp;
+
+public class ManagerGame {
+    public void playChess() {
+    }
+
+    //А если пользователь захочет поиграть в другую игру или список игр расширится, придётся данный класс ИЗМЕНЯТЬ, а не расширять.
+
+}
