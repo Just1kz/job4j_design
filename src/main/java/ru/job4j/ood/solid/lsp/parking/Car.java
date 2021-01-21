8d8acaa1fb5063b6c8be8657b719f4a7ef683e62
Just1lz <exelent.pride@rambler.ru>
2021-01-21 15:21:39 +0700

    2. Парковка машин [#409398]

--- src/main/java/ru/job4j/ood/solid/lsp/parking/Car.java
+++ src/main/java/ru/job4j/ood/solid/lsp/parking/Car.java
@@ -1,7 +1,7 @@
 package ru.job4j.ood.solid.lsp.parking;
 
 public interface Car {
-    String setPlace();
+    int size();
 
     String registrationCode();
 }
--- src/main/java/ru/job4j/ood/solid/lsp/parking/Parking.java
+++ src/main/java/ru/job4j/ood/solid/lsp/parking/Parking.java
@@ -1,7 +1,7 @@
 package ru.job4j.ood.solid.lsp.parking;
 
 public interface Parking {
-    boolean createParking();
+    Car getCar(Car car);
 
     void addCar(Car car);
 }
