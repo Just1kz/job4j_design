1ad61db6d2af6b6a30c865de34b5c2fe6c2c0bed
Just1lz <exelent.pride@rambler.ru>
2021-01-25 13:12:44 +0700

    3.1. Прерывание блокированной нити. [#409574]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,27 +1,32 @@
 package ru.job4j.concurrent;
 
 public class Wget {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(
                 () -> {
-                    try {
-                        System.out.println("Start loading ... ");
-                        int index = 0;
-                        for (int x = 0; x <= 100; x++) {
-                            System.out.print("\rLoading : " + index  + "%");
-                            Thread.sleep(1000);
-                            index++;
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println("Start loading ... ");
+                            int index = 0;
+                            for (int x = 0; x <= 100; x++) {
+                                System.out.print("\rLoading : " + index + "%");
+                                Thread.sleep(1000);
+                                index++;
+                            }
+                            System.out.println("");
+                            Thread.currentThread().getState();
+                            System.out.println("Loaded.");
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            e.printStackTrace();
                         }
-                        System.out.println("");
-                        Thread.currentThread().getState();
-                        System.out.println("Loaded.");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
                     }
                 }
         );
         System.out.println(thread.getState());
         thread.start();
+        Thread.sleep(3000);
+        thread.interrupt();
         System.out.println(thread.getState());
     }
 }
