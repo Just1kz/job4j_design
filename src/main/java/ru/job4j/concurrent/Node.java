fb60da75e2d358ca03e9f947065899e4db60fc8b
Just1lz <exelent.pride@rambler.ru>
2021-01-26 22:43:54 +0700

    3. Immutable объекты [#409580]

--- src/main/java/ru/job4j/concurrent/Node.java
+++ src/main/java/ru/job4j/concurrent/Node.java
@@ -4,22 +4,19 @@ import net.jcip.annotations.Immutable;
 
 @Immutable
 public class Node<T> {
-    private Node next;
-    private T value;
+    private final Node next;
+    private final T value;
+
+    public Node(Node next, T value) {
+        this.next = next;
+        this.value = value;
+    }
 
     public Node getNext() {
         return next;
     }
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
     public T getValue() {
         return value;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
     }
 }
