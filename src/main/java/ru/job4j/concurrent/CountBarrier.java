5be0c92b44db68df9bafb2c016fc3bca948e0786
Just1lz <exelent.pride@rambler.ru>
2021-02-01 15:37:20 +0700

    0. Управление нитью через wait. [#409569]

---
+++ src/main/java/ru/job4j/concurrent/CountBarrier.java
@@ -0,0 +1,48 @@
+package ru.job4j.concurrent;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void count() throws InterruptedException {
+        synchronized (monitor) {
+            while (count != total) {
+                count++;
+                System.out.println(count);
+                Thread.sleep(1000);
+            }
+            monitor.notifyAll();
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count != total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+    //1. Разработайте класс, который блокирует выполнение по условию счетчика.
+    //Переменная total содержит количество вызовов метода count().
+    //Нити, которые выполняют метод await, могут начать работу если ->
+    // поле count == total. Если оно не равно, то нужно перевести нить в состояние wait.
+}
---
+++ src/main/java/ru/job4j/concurrent/MultiCounter.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+public class MultiCounter {
+    public static void main(String[] args) {
+        CountBarrier countBarrier = new CountBarrier(10);
+        Thread masterShifu = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    try {
+                        countBarrier.count();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                },
+                "MasterShifu"
+        );
+        Thread panda = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    countBarrier.await();
+                    System.out.println("Блокировка c потока - " + Thread.currentThread().getName()
+                            + "СНЯТА, вперёёёёёд! =)");
+                },
+                "Panda"
+        );
+        masterShifu.start();
+        panda.start();
+    }
+}
